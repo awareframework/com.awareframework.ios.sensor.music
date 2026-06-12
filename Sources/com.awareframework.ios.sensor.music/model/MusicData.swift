8f2b70aafb95aa9b038177e57a9c0ba4abcabda9
Yuuki Nishiyama <yuukin@iis.u-tokyo.ac.jp>
2026-06-11 18:23:11 -0700

    Rename DB table to ios_music
    - Rename databaseTableName from MusicData to ios_music

--- Sources/com.awareframework.ios.sensor.music/model/MusicData.swift
+++ Sources/com.awareframework.ios.sensor.music/model/MusicData.swift
@@ -3,7 +3,7 @@ import GRDB
 import com_awareframework_ios_core
 
 public struct MusicData: BaseDbModelSQLite {
-    public static let databaseTableName = "musicData"
+    public static let databaseTableName = "ios_music"
     public static let TABLE_NAME = databaseTableName
 
     public var timezone: Int = AwareUtils.getTimeZone()
